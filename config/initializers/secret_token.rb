1e0651fa07edcc38f48662b004e5ba21620d6676
Rex He <prohenghzh@gmail.com>
2012-05-18 18:52:36 +1000

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RexLab::Application.config.secret_token = '639362ed5fb1804c4927d29bb1c0a3e5a48a933e4d570e4a4e5a161e7cec658ce13c77192204ee8eac615c7264414cf94f3e9b24d616d45c175be16487b9d7ab'
